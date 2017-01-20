8cfde3259202f606e2ab0d856f1e5650723f4492
Baptiste Lestavel <baptiste.lestavel@hei.fr>
2017-01-20 13:02:54 +0100

    Fin XML enregistrement

--- app/src/main/java/com/example/lestavelbaptiste/mapmove/RegisterActivity.java
+++ app/src/main/java/com/example/lestavelbaptiste/mapmove/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.lestavelbaptiste.mapmove;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText etPseudo =(EditText) findViewById(R.id.etPseudo);
+        final EditText etMail =(EditText) findViewById(R.id.etMail);
+        final EditText etPassword =(EditText) findViewById(R.id.etPassword);
+
+        final Button bEnregistrer = (Button) findViewById(R.id.bEnregistrer);
+
+
     }
 }
