c2ed816db76e1f8c92f168d755b83c12d9b880ca
Benjamin Mangeot <benjaminmangeot@live.fr>
2017-01-20 11:06:06 +0100

    creation activité

--- app/src/main/java/com/example/lestavelbaptiste/mapmove/ConnectionActivity.java
+++ app/src/main/java/com/example/lestavelbaptiste/mapmove/ConnectionActivity.java
@@ -1,7 +1,12 @@
 package com.example.lestavelbaptiste.mapmove;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ConnectionActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class ConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
+        final EditText id =(EditText) findViewById(R.id.etId);
+        final EditText password = (EditText) findViewById(R.id.etPassword);
+
+        final Button connection = (Button) findViewById(R.id.bConnection);
+
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegister);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(ConnectionActivity.this, RegisterActivity.class);
+                ConnectionActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
